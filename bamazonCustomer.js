6313fb4ca9179f1c6c9c076d9bcb6733531f0b98
Rachel Puder <rcompuder@gmail.com>
2019-09-09 14:42:27 -0400

    added input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -31,3 +31,28 @@ function BidItem(){
     console.log(query.sql);
 
 }
+function Ask(){
+    inquirer
+        .prompt([
+            {
+                name: "whatItem",
+                type: "list",
+                message: "What is the ID of the product you would like to buy?",
+                choices: ["1","2","3","4","5","6","7","8","10"]
+            },
+            {
+                name: "whatItem",
+                type: "list",
+                message: "How Many Units?",
+                choices: ["1","2","3","4","5","6","7","8","10"]
+            }
+            ])
+        
+            .then(function(inquirerResponse) {
+                // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+                if (inquirerResponse.item==="Bid An Item") {
+                  console.log(inquirerResponse.item + "this is IF");
+                  BidItem()
+        };
+
+    
