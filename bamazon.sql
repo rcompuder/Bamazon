39cac2a9e99b96d99df53776edac1470347c2495
Rachel Puder <rcompuder@gmail.com>
2019-09-09 13:37:15 -0400

    edditd items

--- bamazon.sql
+++ bamazon.sql
@@ -6,19 +6,40 @@ USE bamazon_db;
 
 CREATE TABLE products (
   id INT NOT NULL AUTO_INCREMENT,
-  item_id INTEGER(50),
+  item_id INTEGER(10),
   product_name VARCHAR(45) NULL,
   department_name VARCHAR(45) NULL,
-  price Integer(45) NULL,
-  stock_quantity VARCHAR(45) NULL,
+  price Integer(10) NULL,
+  stock_quantity Integer(10),
   PRIMARY KEY (id)
 );
 
 INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
-VALUES ("1","purse", "womens", "", "projects");
+VALUES (1,"purse", "womens", 67, "projects");
 
-INSERT INTO products (items, tasks, jobs, projects)
-VALUES ("2","purse2", "akbfk2", "jobs2", "projects2");
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (2,"purse", "womens", 100, "projects");
 
-INSERT INTO products (items, tasks, jobs, projects)
-VALUES ("purse3", "akbfk3", "jobs3", "projects3");
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (3,"purse", "womens", 7, "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (4,"purse", "womens", 6, "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (5,"purse", "womens", 670, "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (6,"purse", "womens", 15 , "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (7,"purse", "womens", 25, "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (8,"purse", "womens", 67, "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (9,"purse", "womens", 67, "projects");
+
+INSERT INTO products (item_id, product_name , department_name, price,stock_quantity):
+VALUES (10,"purse", "womens", 67, "projects");
