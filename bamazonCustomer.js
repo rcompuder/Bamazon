c8342902c63ef2ca323641825bbb0a3be3cb0420
Rachel Puder <rcompuder@gmail.com>
2019-09-09 13:17:06 -0400

    commit initial js

---
+++ bamazonCustomer.js
@@ -0,0 +1,33 @@
+var inquirer = require("inquirer");
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Bentley2019",
+  database: "bamazon_db"
+});
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    Ask()
+  });
+  
+  function afterConnection() {
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+  }
+function BidItem(){
+    console.log(query.sql);
+
+}
